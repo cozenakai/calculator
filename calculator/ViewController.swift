3f9f548d08d00aa7e056f07eb50ebb037e6dbc7a
cozenakai <masaki2970@icloud.com>
2020-05-16 08:22:32 +0900

    first commit

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -17,7 +17,33 @@ class ViewController: UIViewController {
     var number3: Int = 0
     
     var ope: Int = 0
-
+    
+    
+    @IBAction func select3(){
+        number1 = number1*10 + 3
+        label.text = String(number1)
+    }
+    @IBAction func select4(){
+        number1 = number1*10 + 4
+        label.text = String(number1)
+    }
+    @IBAction func plus(){
+        number2 = number1
+        number1 = 0
+//        ope = 1
+    }
+    @IBAction func equal(){
+            number3 = number2 + number1
+//            number3 = number2 - number1
+//        }else if ope == 3{
+//            number3 = number2 * number1
+//        }else if ope == 4{
+//            number3 = number2 / number1
+        
+        label.text = String(number3)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
